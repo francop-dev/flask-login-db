8bf9dd90fa85e30c8671cb28a88b03877b4a7b78
francop-dev <francoperuil.dev@gmail.com>
2024-06-06 14:56:26 -0300

    login con flask, conexion a mysql, chequeo de password con hash y csrf con token

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request,flash
 from flask_mysqldb import MySQL
+from flask_wtf import CSRFProtect
 from config import config
 from flask_login import LoginManager,login_user,logout_user,login_required
 #models
@@ -10,6 +11,7 @@ from models.entities.User import User
 
 app =Flask(__name__)
 
+csrf = CSRFProtect()
 db = MySQL(app)
 login_manager_app = LoginManager(app)
 
@@ -51,8 +53,23 @@ def logout():
 def home():
     return render_template('home.html')
 
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1>esta es una vista protegida, solo para usuarios logueados.</h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Pagina no encontrada</h1>", 404
+
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
+    csrf.init_app(app)
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_404)
     app.run()
     
 
